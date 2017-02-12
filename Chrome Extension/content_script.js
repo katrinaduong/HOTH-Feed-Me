510f3d66cdfe140b2d5f5c156fd2042d5d37086a
Nicole Wong <n.kc.wong.123@gmail.com>
2017-02-11 19:41:08 -0800

    removed walk function

--- Chrome Extension/content_script.js
+++ Chrome Extension/content_script.js
@@ -1,50 +1,6 @@
-walk(document.body); 
-
 var highlightedWord;
 var foodwords;
 initializeData();
-
-function walk(node)
-{
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-
-	var child, next;
-
-	/*if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
-	    || node.classList.indexOf('ace_editor') > -1) {
-		return;
-	}*/
-
-	switch ( node.nodeType )
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child )
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
-
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
-}
-
-function handleText(textNode)
-{
-	var v = textNode.nodeValue;
-
-	v = v.replace(/\bFacebook\b/g, "Google");
-  v = v.replace(/\bfacebook\b/g, "google");
-
-	textNode.nodeValue = v;
-}
 
 document.addEventListener('mouseup', function() {
 	highlightedWord = window.getSelection().toString()
